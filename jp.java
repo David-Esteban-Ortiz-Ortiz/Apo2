69a43c543e8584a1ae30602196589804b7e869f2
Sebastian Moreno <sebastianfelip.12@gmail.com>
2023-10-20 00:31:21 -0500

    2. Se agrego un menu realizado con switch

--- jp.java
+++ jp.java
@@ -1,6 +1,6 @@
-### proceso con funciones en eclipse 
+// 1. Proceso con funciones en eclipse 
 
-//metodo con funciones que busca determinar cualquier numero factorial 
+// Metodo con funciones que busca determinar cualquier numero factorial 
 
 public class news {
 
@@ -39,5 +39,88 @@ public class news {
 		
    }
 }
+
+// 2. Menu con la utilizacion de switch 
+
+public static void main(String[] args) {
+		
+		int numero =0;
+		while(numero !=4) {
+			
+		System.out.println("TABLA PARA INSERTAR \n 1. DISTANCIA \n 2. PROMEDIO \n 3. PARTIDOS \n 4. SALIENDO ");
+	
+		Scanner n1=new Scanner (System.in);
+		System.out.println("inserte un numero: "); 
+		numero=n1.nextInt();
+		String opc ="";
+		
+		
+		switch (numero) {
+		
+		case 1: 
+		opc="OPCION 1"; 
+		System.out.println(opc);
+			
+			int resultado=0;
+			int velocidad=20;
+			int tiempo=5;
+			
+			resultado= tiempo*velocidad;
+			System.out.println("resultado de distancia: " +resultado); 
+		break; 
+		
+		case 2: 
+			opc="OPCION 2";
+			System.out.println(opc);
+			
+			Scanner n11=new Scanner (System.in);
+			float not1=n11.nextFloat();
+			
+			Scanner n2=new Scanner (System.in);
+			float not2=n2.nextFloat();
+			
+			Scanner n3=new Scanner (System.in);
+			float not3=n3.nextFloat();
+			
+			double promedio=0;
+			
+			promedio = (not1+not2+not3)/3;
+			System.out.println("resultado del promedio: " +promedio);
+		break;
+		
+		case 3:
+			opc="OPCION 3";
+			System.out.println(opc);
+			
+			double totalpuntos=0;
+			
+			int pganados=5;
+			int pperdidos=3;
+			int pempatados=2;
+			
+			totalpuntos= pganados+pperdidos+pempatados;
+			System.out.println("total de puntos: "+totalpuntos);
+			
+			break;
+			
+		case 4:
+			opc="OPCION 4";
+			System.out.println(opc);
+			
+		System.out.println("SISTEMA CERRADO ");
+	
+		break; 
+	default:
+		System.out.println("EL NUMERO NO SE ENCUENTRA EN LA PANTALLA  ");	
+		
+		break;
+		
+		}
+		
+	
+	}
+	}
+
+
 
 
